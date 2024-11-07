717e58eb9b5e3308b0258efade4c2e4330de3131
Muhammadarif98 <67822369+Muhammadarif98@users.noreply.github.com>
2024-11-07 21:52:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/themes/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.themes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.themes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
